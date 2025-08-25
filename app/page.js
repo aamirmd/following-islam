5340fbba80a67a38b470420940b334c1c44c6bf2
aamirmd <aamirmd105@gmail.com>
2025-08-25 19:41:18 -0400

    Add basmalah image

--- app/page.js
+++ app/page.js
@@ -2,7 +2,14 @@ import styles from "./page.module.css";
 
 export default function Home() {
     return (
-        <div className="container-fluid mx-2 my-1">
+        <div className="container mx-2 my-1">
+            <div className="container d-flex justify-content-center my-1">
+                <img
+                    className="w-50"
+                    alt="Basmala - (image credit to vecteezy)"
+                    src="https://static.vecteezy.com/system/resources/previews/025/345/106/non_2x/bismillah-in-the-name-of-allah-that-occurs-at-the-very-start-of-the-qur-an-one-of-the-most-important-phrases-in-islam-and-is-used-by-muslims-people-before-starting-pray-and-good-deeds-free-vector.jpg"
+                ></img>
+            </div>
             <div className="my-2">Welcome to followingislam.org</div>
             <div className="my-2">
                 We plan to offer a variety of services such as:
@@ -15,7 +22,7 @@ export default function Home() {
                     <li>Tailoring & Clothing</li>
                 </ul>
             </div>
-            <div className="container d-flex justify-content-center">
+            <div className="container">
                 <img
                     className="w-50"
                     src="https://home.csulb.edu/~cwallis/100/worldreligions/Mecca.jpg"
