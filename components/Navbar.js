5dec97101f5181bbb77aa4bc741968d179b928b2
aamirmd <aamirmd105@gmail.com>
2025-08-25 19:09:29 -0400

    Better navbar styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,27 +1,50 @@
-// components/Navbar.js
+"use client";
+
 import Link from "next/link";
+import styles from "./Navbar.module.css";
+
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+    const pathname = usePathname();
+    const itemSelectedBootstrap = "active fw-bold text-info";
+    const itemDefaultBootstrap = "text-light";
+
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <div
-                    className="collapse navbar-collapse"
-                    // id="navbarSupportedContent"
-                >
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link" href="/">
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" href="/janazah">
-                                Janazah
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+        <nav
+            className="navbar navbar-expand-lg bg-body-tertiary"
+            data-bs-theme="dark"
+        >
+            <div
+                className="container-fluid collapse navbar-collapse"
+                // id="navbarSupportedContent"
+            >
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className={`nav-item mx-0 my-0`}>
+                        <Link
+                            className={`nav-link ${
+                                pathname === "/"
+                                    ? itemSelectedBootstrap
+                                    : itemDefaultBootstrap
+                            }`}
+                            href="/"
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            className={`nav-link ${
+                                pathname === "/janazah"
+                                    ? itemSelectedBootstrap
+                                    : itemDefaultBootstrap
+                            }`}
+                            href="/janazah"
+                        >
+                            My Final Place
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
