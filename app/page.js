1c9976b14e4e4931a0733f81f1607a9b59ae0f45
aamirmd <aamirmd105@gmail.com>
2025-08-25 18:02:00 -0400

    Center makkah picture in home page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
     return (
-        <div>
-            <div>Welcome to followingislam.org</div>
-            <div>
-                We offer a variety of services such as:
+        <div className="container-fluid mx-2 my-1">
+            <div className="my-2">Welcome to followingislam.org</div>
+            <div className="my-2">
+                We plan to offer a variety of services such as:
                 <ul>
                     <li>Janazah</li>
                     <li>Little Muslim</li>
@@ -17,7 +15,12 @@ export default function Home() {
                     <li>Tailoring & Clothing</li>
                 </ul>
             </div>
-            <img src="https://home.csulb.edu/~cwallis/100/worldreligions/Mecca.jpg"></img>
+            <div className="container d-flex justify-content-center">
+                <img
+                    className="w-50"
+                    src="https://home.csulb.edu/~cwallis/100/worldreligions/Mecca.jpg"
+                ></img>
+            </div>
         </div>
     );
 }
