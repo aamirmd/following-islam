81ad1545457cf801c15f7d32bcfd742a22fa2635
aamirmd <aamirmd105@gmail.com>
2025-08-25 17:37:44 -0400

    Change highlighting in Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,11 +11,7 @@ const Navbar = () => {
                 >
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link
-                                className="nav-link active"
-                                aria-current="page"
-                                href="/"
-                            >
+                            <Link className="nav-link" href="/">
                                 Home
                             </Link>
                         </li>
