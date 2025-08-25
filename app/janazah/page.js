243ae0928f8572b5bf8446bfbc341b0f1befd031
aamirmd <aamirmd105@gmail.com>
2025-08-25 17:38:05 -0400

    Use better form styling

--- app/janazah/page.js
+++ app/janazah/page.js
@@ -4,7 +4,7 @@ import styles from "./janazah.module.css";
 const Janazah = () => {
     return (
         <div>
-            <section>
+            <section className="container mx-2 my-1">
                 We offer a janazah service for just $150 per person per year.
                 Here is how it works:
                 <ol>
@@ -17,38 +17,50 @@ const Janazah = () => {
                     <li>We will take care of the rest</li>
                 </ol>
             </section>
-            <div>To sign up, please fill out the form below.</div>
-            <div>
-                <label htmlFor="task" className={styles.textlabel}>
-                    Name
-                </label>
-                <input type="text" id="task" name="task"></input>
+
+            <div className="container mx-2 my-2">
+                To sign up, please fill out the form below.
             </div>
-            <div>
-                <label htmlFor="task" className={styles.textlabel}>
-                    Address
-                </label>
-                <input type="text" id="task" name="task"></input>
+            <div className="container">
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="janazahName" className="form-label">
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control col-sm-1"
+                            id="janazahName"
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="janazahEmail" className="form-label">
+                            Email address
+                        </label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="janazahEmail"
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label
+                            htmlFor="janazahPhoneNumber"
+                            className="form-label"
+                        >
+                            Phone Number
+                        </label>
+                        <input
+                            type="tel"
+                            className="form-control"
+                            id="janazahPhoneNumber"
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
+                </form>
             </div>
-            <div>
-                <label htmlFor="task" className={styles.textlabel}>
-                    Contact No.
-                </label>
-                <input type="text" id="task" name="task"></input>
-            </div>
-            <div>
-                <label htmlFor="task" className={styles.blue}>
-                    Credit card Number
-                </label>
-                <input type="text" id="task" name="task"></input>
-            </div>
-            <div>
-                <label htmlFor="task" className={styles.textlabel}>
-                    Amount: $150
-                </label>
-                <input type="text" id="task" name="task"></input>
-            </div>
-            <button type="button">Pay Now</button>
         </div>
     );
 };
