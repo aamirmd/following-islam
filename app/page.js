97aeaaf6be583e032cdaa363e7d5decb2f4ae6c2
aamirmd <aamirmd105@gmail.com>
2025-08-25 20:04:38 -0400

    Improve list

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 
 export default function Home() {
     return (
-        <div className="container mx-2 my-1">
+        <div className="container-fluid mx-2 my-1 text-center">
             <div className="container d-flex justify-content-center my-1">
                 <img
                     className="w-50"
@@ -10,19 +10,22 @@ export default function Home() {
                     src="https://static.vecteezy.com/system/resources/previews/025/345/106/non_2x/bismillah-in-the-name-of-allah-that-occurs-at-the-very-start-of-the-qur-an-one-of-the-most-important-phrases-in-islam-and-is-used-by-muslims-people-before-starting-pray-and-good-deeds-free-vector.jpg"
                 ></img>
             </div>
-            <div className="my-2">Welcome to followingislam.org</div>
+            <div className="my-1 h4">Welcome to followingislam.org</div>
             <div className="my-2">
                 We plan to offer a variety of services such as:
-                <ul>
-                    <li>Janazah</li>
-                    <li>Little Muslim</li>
-                    <li>Riba & Debt</li>
-                    <li>Looking for a job</li>
-                    <li>Looking for an employee</li>
-                    <li>Tailoring & Clothing</li>
-                </ul>
+                <div className="d-flex justify-content-center">
+                    <ul className="list-group w-25">
+                        <li className="list-group-item">Janazah</li>
+                        <li className="list-group-item">Little Muslim</li>
+                        <li className="list-group-item">Riba & Debt</li>
+                        <li className="list-group-item">Looking for a job</li>
+                        <li className="list-group-item">
+                            Tailoring & Clothing
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className="container">
+            <div className="container d-flex justify-content-center my-1">
                 <img
                     className="w-50"
                     src="https://home.csulb.edu/~cwallis/100/worldreligions/Mecca.jpg"
