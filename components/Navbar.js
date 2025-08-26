1ec24298061884062307459c05fe911faafeadf6
aamirmd <aamirmd105@gmail.com>
2025-08-25 20:04:47 -0400

    Make navbar sticky

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
 
     return (
         <nav
-            className="navbar navbar-expand-lg bg-body-tertiary"
+            className="navbar navbar-expand-lg bg-body-tertiary sticky-top"
             data-bs-theme="dark"
         >
             <div
