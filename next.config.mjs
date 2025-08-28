f1c6c29fe560908bd5d1343b347471b122ce28fb
aamirmd <aamirmd105@gmail.com>
2025-08-28 16:22:04 -0400

    Remove static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    basePath: "/following-islam",
-    assetPrefix: "/following-islam/",
-};
+const nextConfig = {};
 
 export default nextConfig;
