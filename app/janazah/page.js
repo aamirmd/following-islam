76300e299b96c657f7f383c39942cf5548c626e3
aamirmd <aamirmd105@gmail.com>
2025-08-28 16:41:27 -0400

    Add explanation and quotes

--- app/janazah/page.js
+++ app/janazah/page.js
@@ -4,9 +4,57 @@ import styles from "./janazah.module.css";
 const Janazah = () => {
     return (
         <div className="">
+            <div className="container d-flex justify-content-center">
+                <figure class="text-center mt-3 mb-1 text-info">
+                    <blockquote class="blockquote">
+                        <p>
+                            And do not say about those who are killed in the way
+                            of Allāh, "They are dead." Rather, they are alive,
+                            but you perceive [it] not.
+                        </p>
+                    </blockquote>
+                    <figcaption class="blockquote-footer">
+                        Quran 2:154 <cite>(Saheeh International)</cite>
+                    </figcaption>
+                </figure>
+            </div>
             <section className="container mx-2 mt-4">
-                We offer a janazah service for just $150 per person per year.
-                Here is how it works:
+                In Islam, Janaza refers to the funeral rites performed for a
+                deceased Muslim. These rites are carried out with great care,
+                dignity, and compassion, as a way of honoring the person who has
+                returned to their Creator. The process includes
+                <ol>
+                    <li>Washing and shrouding the body</li>
+                    <li>Offering Salat al-Janazah (funeral prayer)</li>
+                    <li>
+                        Burying the deceased in accordance with Islamic
+                        tradition
+                    </li>
+                </ol>
+                Our service is dedicated to helping families fulfill these
+                important obligations with sincerity and respect, ensuring the
+                final rites are performed according to the Sunnah.
+            </section>
+            <div className="container d-flex justify-content-center">
+                <figure class="text-center mt-3 mb-1 text-info w-50">
+                    <blockquote class="blockquote">
+                        <p>
+                            The Prophet Muhammad (ﷺ) said: “Whoever attends the
+                            Janaza until the prayer is offered for it, then he
+                            will have one Qirat (of reward), and whoever attends
+                            until it is buried, he will have two Qirats.” It was
+                            asked, “What are two Qirats?” He (ﷺ) said: “Like two
+                            great mountains.”
+                        </p>
+                    </blockquote>
+                    <figcaption class="blockquote-footer">
+                        <cite>[Sahih al-Bukhari & Sahih Muslim]</cite>
+                    </figcaption>
+                </figure>
+            </div>
+            <section className="container mx-2 mt-4">
+                Our service costs $200 per person per year. Here is how it
+                works:
                 <ol>
                     <li>Sign up with information of the individual</li>
                     <li>Pledge to pay $150 per year</li>
